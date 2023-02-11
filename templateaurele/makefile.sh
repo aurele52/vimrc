828e5724c088a91fc968d5b32d2b6e9975615284
audreyer <aureledreyer67@outlook.fr>
2023-02-12 00:26:20 +0100

    arg

--- templateaurele/makefile.sh
+++ templateaurele/makefile.sh
@@ -1,23 +1,38 @@
 echo >Makefile "NAME= $1"
-echo >>Makefile "SRC= $1.cpp"
+echo >>Makefile "SRC=	main.cpp"
 echo >>Makefile 'CC= c++'
 echo >>Makefile 'FLAG= -g -Wall -Wextra -Werror -std=c++98'
 echo >>Makefile
-echo -e >>Makefile 'all:\t$(NAME)'
+echo >>Makefile 'all:	$(NAME)'
 echo >>Makefile
 
-echo -e >>Makefile '$(NAME):	$(SRC)'
-echo -e >>Makefile '	$(CC) $(FLAG) -o $@ $^'
+echo >>Makefile '$(NAME):	$(SRC)'
+echo >>Makefile '	$(CC) $(FLAG) -o $@ $^'
 
 echo >>Makefile
-echo -e >>Makefile 'clean:'
+echo >>Makefile 'clean:'
 echo >>Makefile
 
-echo -e >>Makefile 'fclean: clean'
-echo -e >>Makefile '	rm -rf $(NAME)'
+echo >>Makefile 'fclean:	clean'
+echo >>Makefile '	rm -rf $(NAME)'
 echo >>Makefile
 
-echo -e >>Makefile 're: fclean all'
+echo >>Makefile 're:	fclean all'
 echo >>Makefile
 
-echo -e >>Makefile '.PHONY: all clean fclean re test'
+echo >>Makefile '.PHONY: all clean fclean re test'
+
+echo >main.hpp "#ifndef MAIN_H"
+echo >>main.hpp	"# define MAIN_H"
+echo >>main.hpp
+echo >>main.hpp "#include <string>"
+echo >>main.hpp "#include <iostream>"
+echo >>main.hpp
+echo >>main.hpp "#endif"
+
+echo >main.cpp "#include \"main.hpp\""
+echo >>main.cpp ""
+echo >>main.cpp "int	main()"
+echo >>main.cpp "{"
+echo >>main.cpp 
+echo >>main.cpp "}"
